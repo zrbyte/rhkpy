8d8dee81cd5b1d78db7683cfec33e79d2d5e8774
zrbyte <zrbyte@users.noreply.github.com>
2024-01-17 10:22:51 +0100

    added version.py

--- rhkpy/__init__.py
+++ rhkpy/__init__.py
@@ -1,3 +1,4 @@
+from .version import __version__
 from .rhkpy_loader import *
 from .rhkpy_process import *
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "rhkpy",
-    version = "1.3.4",
+    version = "1.3.5",
     packages = find_packages(),
     # python_requires = '>=3.10',
     install_requires = [
