bd169ebcb6d38bd72d3b4223b2625ba6bfb6b519
zrbyte <zrbyte@users.noreply.github.com>
2024-06-12 15:35:01 +0200

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "rhkpy",
-    version = "1.3.6",
+    version = "1.3.7",
     packages = find_packages(),
     # python_requires = '>=3.10',
     install_requires = [
