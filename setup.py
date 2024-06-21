11394155a555697f439b42740d22e6ec1351deda
zrbyte <zrbyte@users.noreply.github.com>
2024-06-21 15:46:54 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "rhkpy",
-    version = "1.3.7",
+    version = "1.3.8",
     packages = find_packages(),
     # python_requires = '>=3.10',
     install_requires = [
